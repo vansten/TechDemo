117cf04b9652e687d72177f96536b924ab8530d7
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2017-02-17 19:49:57 +0100

    [+] Base project

---
+++ TechDemo/Source/TechDemo/TechDemo.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TechDemo.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TechDemo, "TechDemo" );
---
+++ TechDemo/Source/TechDemo/TechDemoGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TechDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TECHDEMO_API ATechDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
